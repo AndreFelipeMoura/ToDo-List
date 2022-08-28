626e3bfe9c1501a63bdea425bc8b02e1faab7a6a
Andre Moura <andre.fa.moura@outlook.com>
2022-08-28 14:52:12 -0300

    inputs/button das tarefas

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,10 +4,50 @@ import Header from "../header/Header";
 
 
 function List() {
-    return(
-    <div>
+return(
+<>
     <Header />
-    </div>
+    <div className="container">
+        <section className="list">
+
+            <h2 className=""> NOVA TAREFA </h2>
+            <form className="form-input-add">
+                <div>
+                    <input placeholder='adicione nova tarefa' type="text" />
+                    <p> Descrição: </p>
+                    <textarea />
+
+
+                    <button> ADICIONAR TAREFA </button>
+                    </div>
+            </form>
+        
+    </section>
+    <section>
+    <h2 className=""> TAREFAs </h2>
+            <form className = "form-input-task">
+                <div>
+                    <ul>
+                        <li>
+                            <div>
+                                <label>
+                                    <input></input>
+                                </label>
+                            </div>
+                        </li>
+                    </ul>
+                    <input type="checkbox"  />
+                    <p> Descrição: </p>
+                    <textarea />
+                    
+
+                    </div>
+            </form>
+
+    </section>
+
+    </div></>
+     
     )
 }
 
